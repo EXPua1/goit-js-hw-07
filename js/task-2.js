2cc07996354cbf4fc0bc9579ff7f0d0ab7947b82
unknown <badsnake1@gmail.com>
2024-07-03 17:47:09 +0300

    ddd

--- js/task-2.js
+++ js/task-2.js
@@ -29,7 +29,7 @@ const gallery = document.querySelector(".gallery");
 const galleryItems = images
   .map(
     ({ url, alt }) =>
-      `<li class = "gallery-item"><img src = "${url}" alt = "${alt}" class = "gallery-image"></li>`
+      `<li class="gallery-item"><img src="${url}" alt="${alt}" class="gallery-image"></li>`
   )
   .join("");
 
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const registerForm = document.querySelector(".login-form");
+registerForm.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  const passwordRegex = /^(?=.*[A-Z]).{6,}$/;
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+  } else if (!passwordRegex.test(password)) {
+    alert(
+      "Password must contain at least 6 characters with at least one uppercase letter"
+    );
+  } else {
+    const userData = {
+      email: email,
+      password: password,
+    };
+    console.log(userData);
+    form.reset();
+  }
+}
