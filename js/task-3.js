feb48abbe6c624b6050d290784a7496e85d83c1f
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2024-07-04 15:17:25 +0300

    fix task 3

--- js/task-3.js
+++ js/task-3.js
@@ -2,5 +2,18 @@ const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
+  const inputValue = event.currentTarget.value.trim()
+  // if (inputValue === '' || inputValue === ' ') {
+  //   output.textContent = 'Anonymous'
+    
+  // } else {
+  //   output.textContent = inputValue
+  // }
+output.textContent = inputValue === '' ? 'Anonymous' : inputValue
+
+    
+ 
+
+  
 });
+ 
